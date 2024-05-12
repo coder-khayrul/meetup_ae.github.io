d408024fb0e893f63f371a16201ee925ed3bf662
khayrul200822 <khayrulislam200822@gmail.com>
2024-05-13 00:03:32 +0600

    home page uploaded

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+//Swiper Code
+var swiper = new Swiper(".hero_swiper_slider", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next-hero",
+        prevEl: ".swiper-button-prev-hero",
+    },
+});
